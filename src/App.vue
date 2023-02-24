487077e6a41a3ac0cbc200bcdb57e44950d234ff
skoenfeld <info@skoen.red>
2023-02-24 08:34:11 +0100

    git init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue' // Cannot resolve symbol 'defineComponent / ref'
+export default defineComponent({
+  setup() {
+    const devMode = ref(import.meta.env.DEV)
+
+    return {
+      devMode,
+    }
+  },
+})
+</script>
+
+<template>
+  {{ devMode }}
+
+  <RouterView />
+</template>
